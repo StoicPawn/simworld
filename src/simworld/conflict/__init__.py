ac3b56527c68c6ccd820c86e193f5c22e6175446
StoicPawn <151366316+StoicPawn@users.noreply.github.com>
2026-08-09 12:53:20 +0200

    Export conflict primitives

---
+++ src/simworld/conflict/__init__.py
@@ -0,0 +1,3 @@
+from simworld.conflict.model import ConflictContext, ConflictDecision, choose_conflict_action, conflict_pressure
+
+__all__ = ["ConflictContext", "ConflictDecision", "choose_conflict_action", "conflict_pressure"]
