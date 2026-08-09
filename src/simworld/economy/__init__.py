4d01a6ce2cc35f242be1ff5078d0ad627158a268
StoicPawn <151366316+StoicPawn@users.noreply.github.com>
2026-08-09 12:53:26 +0200

    Export agriculture primitives

---
+++ src/simworld/economy/__init__.py
@@ -0,0 +1,3 @@
+from simworld.economy.agriculture import GRAINS, PULSES, TUBERS, CropProfile, crop_suitability, harvest_yield
+
+__all__ = ["CropProfile", "GRAINS", "PULSES", "TUBERS", "crop_suitability", "harvest_yield"]
